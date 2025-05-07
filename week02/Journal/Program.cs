67219f994fc669aa59aaa88e9d3c4f1055c8003c
temps07 <emperorvikkie@gmail.com>
2025-05-07 09:21:45 +0100

    journal

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Welcome to the Journal Program!");
+        string userInput = "";
+        while (userInput != "5");
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ")
+            string userResponse = Console.ReadLine();
+            if (userResponse == "1");
+            {
+                Random randomGenerator = new Random();
+                int magicNumber = randomGenerator.Next(1, 101);
+            }
+        }
     }
 }
